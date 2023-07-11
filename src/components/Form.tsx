2cb5ed28e9953f7781dbaa901e19aedc3404e462
Felipe Duarte <feduarte.dev@gmail.com>
2023-07-11 16:36:31 -0300

    requisito 5 pronto

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -11,10 +11,16 @@ const INITIAL_STATE = {
   url: '',
 };
 function Form({ formStatus }: FormType) {
+  const specialChars = /[`!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?~]/;
   const [btnStatus, setBtnStatus] = useState(true);
   const [formValues, setFormValues] = useState(INITIAL_STATE);
   const { serviceName, login, password, url } = formValues;
-
+  const [lesserChar, setLesserChar] = useState(false);
+  const [moreChar, setMoreChar] = useState(true);
+  const [letterNumber, setLetterNumber] = useState(false);
+  const [specialChar, setSpecialChar] = useState(false);
+  const valid = 'valid-password-check';
+  const invalid = 'invalid-password-check';
   function handleChange(event: React.ChangeEvent<HTMLInputElement
   | HTMLSelectElement | HTMLTextAreaElement>) {
     const { name, value } = event.target;
@@ -24,17 +30,26 @@ function Form({ formStatus }: FormType) {
     });
   }
   function buttonEnable() {
-    const specialChars = /[`!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?~]/;
-    if (serviceName.length > 0
-        && login.length > 0
-        && password.length > 8 && password.length < 16
-        && /\d/.test(password)
-        && specialChars.test(password)
-        && password.match(/[a-zA-Z]/)) {
-      setBtnStatus(false);
-    } else {
-      setBtnStatus(true);
-    }
+    return serviceName.length > 0
+    && login.length > 0
+    && lesserChar && moreChar
+    && letterNumber
+    && specialChar
+      ? setBtnStatus(false)
+      : setBtnStatus(true);
+  }
+
+  function lesserCharCheck() {
+    return password.length > 7 ? setLesserChar(true) : setLesserChar(false);
+  }
+  function moreCharCheck() {
+    return password.length < 15 ? setMoreChar(true) : setMoreChar(false);
+  }
+  function letterNumberCheck() {
+    return password.match(/[a-zA-Z]/) && /\d/.test(password) ? setLetterNumber(true) : setLetterNumber(false);
+  }
+  function specialCharCheck() {
+    return specialChars.test(password) ? setSpecialChar(true) : setSpecialChar(false);
   }
 
   return (
@@ -66,10 +81,43 @@ function Form({ formStatus }: FormType) {
           type="password"
           name="password"
           value={ password }
-          onChange={ handleChange }
+          onChange={ (e) => {
+            handleChange(e);
+            lesserCharCheck(); moreCharCheck(); letterNumberCheck(); specialCharCheck();
+          } }
+
         />
       </label>
+      <div>
+        <p
+          className={ lesserChar
+            ? valid : invalid }
+        >
+          Possuir 8 ou mais caracteres
 
+        </p>
+        <p
+          className={ moreChar
+            ? valid : invalid }
+        >
+          Possuir até 16 caracteres
+
+        </p>
+        <p
+          className={ letterNumber
+            ? valid : invalid }
+        >
+          Possuir letras e números
+
+        </p>
+        <p
+          className={ specialChar
+            ? valid : invalid }
+        >
+          Possuir algum caractere especial
+
+        </p>
+      </div>
       <label>
         URL
         <input
